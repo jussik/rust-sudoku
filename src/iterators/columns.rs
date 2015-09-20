40439585b97f6041abd14797a71e170eda3d0193
Jussi Kosunen <deathcarrot@gmail.com>
2015-09-20 18:32:54 +0300

    new column iterator

--- src/iterators/columns.rs
+++ src/iterators/columns.rs
@@ -1,56 +1,25 @@
-/// Iterates each column's cell adjacencies
-///
-/// Emits a tuple of two cell locations
-/// Only emits each pair of cells once, i.e. (3,5) but not (5,3)
-pub struct ColumnIterator {
-    tgt: usize,
-    col: usize,
-    row: usize,
-    new_col: bool
-}
-impl ColumnIterator {
-    pub fn new() -> ColumnIterator {
+use super::base::IteratorBase;
+
+/// Iterates each row's cell adjacencies
+pub struct Columns;
+impl Columns {
+    pub fn iter() -> ColumnIterator {
         ColumnIterator {
-            tgt: 0,
-            col: 0,
-            row: 0,
-            new_col: true
+            iter: IteratorBase::new()
         }
     }
+}
+pub struct ColumnIterator {
+    iter: IteratorBase
 }
 impl Iterator for ColumnIterator {
     type Item = (usize, usize);
     fn next(&mut self) -> Option<(usize, usize)> {
-        /*
-         *  for tgt in (0..81) {
-         *      let col = tgt % 9;
-         *      for row in (tgt / 9 + 1 ..9) {
-         *          yield (tgt, row * 9 + col);
-         *      }
-         *  }
-         */
-        loop {
-            if self.new_col {
-                if self.tgt > 80 {
-                    // all columns done
-                    return None;
-                } else {
-                    // check next column
-                    self.col = self.tgt % 9;
-                    self.row = self.tgt / 9;
-                    self.new_col = false;
-                }
-            } else {
-                if self.row >= 8 {
-                    // past last cell in column, go to next
-                    self.tgt += 1;
-                    self.new_col = true;
-                } else {
-                    // send cell
-                    self.row += 1;
-                    return Some((self.tgt, self.row * 9 + self.col));
-                }
-            }
+        let it = &mut self.iter;
+        if it.step() {
+            Some(((it.cell % 9) * 9 + it.major, it.minor * 9 + it.major))
+        } else {
+            None
         }
     }
 }
