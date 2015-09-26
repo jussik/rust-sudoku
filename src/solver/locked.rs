dc72ef617802f7b9040fa54df0254d9ee90970cc
Jussi Kosunen <deathcarrot@gmail.com>
2015-09-26 20:00:21 +0300

    locked solver loops properly

--- src/solver/locked.rs
+++ src/solver/locked.rs
@@ -21,44 +21,43 @@ fn run(args: SolverArgs, func: LocFn, box_func: LocFn) {
     let tx = args.tx;
     let is_done = args.is_done;
     let mut changed = false;
-    for b in 0..9 {
-        // find any values that are only in one row/column of this box
-        let box_offset = box_loc(b, 0); // left top cell of box
-        let mut poss: [u16; 3] = [0; 3]; // all possibles in each row/column
-        for major in 0..3 {
-            for minor in 0..3 {
-                let i = box_offset + func(major, minor);
-                let cell = *grid[i].read().unwrap();
-                if cell.value == -1 {
-                    poss[major] |= cell.possible;
+    loop {
+        for b in 0..9 {
+            // find any values that are only in one row/column of this box
+            let box_offset = box_loc(b, 0); // left top cell of box
+            let mut poss: [u16; 3] = [0; 3]; // all possibles in each box-major
+            for major in 0..3 {
+                for minor in 0..3 {
+                    let i = box_offset + func(major, minor);
+                    let cell = *grid[i].read().unwrap();
+                    if cell.value == -1 {
+                        poss[major] |= cell.possible;
+                    }
                 }
             }
-        }
-        for p in 0..3 {
-            // uniq is bits only in poss[p]
-            let uniq = poss[p] & !(poss[(p + 1) % 3] | poss[(p + 2) % 3]);
-            if uniq != 0 {
-                //println!("uniq: {:09b} in minor {}", uniq, p);
-                let b_maj = (b / 3) * 3;
-                //println!("box {} (in maj {})", b, b_maj/3);
-                for po in 1..3 {
-                    // each boxes to remove possibles from
-                    let bo = (b_maj + box_func(b, po)) % 9;
-                    //println!("others: {}", bo);
-                    let box_offset = box_loc(bo, 0);
-                    for minor in 0..3 {
-                        let i = box_offset + func(p, minor);
-                        //println!("remove from {}", i);
-                        let mut cell = grid[i].write().unwrap();
-                        cell.possible &= !uniq;
+            for p in 0..3 {
+                // uniq is bits only in poss[p]
+                let uniq = poss[p] & !(poss[(p + 1) % 3] | poss[(p + 2) % 3]);
+                if uniq != 0 {
+                    let b_maj = (b / 3) * 3;
+                    for po in 1..3 {
+                        // each boxes to remove possibles from
+                        let bo = (b_maj + box_func(b, po)) % 9;
+                        let box_offset = box_loc(bo, 0);
+                        for minor in 0..3 {
+                            let i = box_offset + func(p, minor);
+                            let mut cell = grid[i].write().unwrap();
+                            cell.possible &= !uniq;
+                            changed |= cell.check_possible();
+                        }
                     }
                 }
             }
         }
-    }
-    tx.send(changed).unwrap();
-    thread::yield_now();
-    if *is_done.read().unwrap() {
-        return;
+        tx.send(changed).unwrap();
+        thread::yield_now();
+        if *is_done.read().unwrap() {
+            return;
+        }
     }
 }
